3860181f4d889c768901d4607141fdd03681064c
adilaroisa <adilaroisa688@gmail.com>
2024-04-04 11:37:03 +0700

    melanjutkan membuat loop

--- LinearSearch04042024/LinearSearch04042024.cpp
+++ LinearSearch04042024/LinearSearch04042024.cpp
@@ -52,7 +52,12 @@ void LinearSearch()
             }
             i++; // step 4
         }
-    }
+
+        if (i == n) // step 5
+            cout << "\n" << item << " not found in the array\n";
+        cout << "\nContinue search (y/n): ";
+        cin >> ch;
+    } while ((ch == 'Y'));
 }
 
 
